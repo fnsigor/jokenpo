67bc3879b31db0ffda16dfd28dffff5ecd5ed5b4
Igor Fernandes <igorfernamdez@gmail.com>
2022-12-28 19:30:55 -0300

    update Game component

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,11 +1,17 @@
 import styled, { css } from 'styled-components'
 
-const Content = styled.img``
+const Content = styled.div`
+    position: absolute;
+    background-color: red;
+    padding: 2rem;
+    border-radius: 50%;
+`
 
 
-
-export function Game ({img}, {value}){
-return(
-    <Content src={img} title={value}/>
-)
+export function Game({ img }, { value }) {
+    return (
+        <Content>
+            <img src={img} title={value} />
+        </Content>
+    )
 }
