09e5f4db8697476d551124a45b6cb6bee2b9e84d
Igor Fernandes <igorfernamdez@gmail.com>
2023-01-08 17:04:12 -0300

    feature: set game icon border-color according to attributes

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -9,21 +9,51 @@ const Content = styled.div`
     border-radius: 50%;
     height: 16rem;
     width: 16rem;
-    border: 2rem solid red;
+    border: 2rem solid;
 
     display: flex;
     align-items: center;
     justify-content: center;
 
+    transition: .1s ease-out;
+
     &:hover{
         cursor: pointer;
     }
+
+    ${({ visible }) => visible === false && css`
+        border: none;
+        background-color: #1a1a1aa0;
+        height: 16rem;
+        width: 16rem;
+
+        img{
+            display:none;
+        }
+    ` }
+
+    ${({ value }) => value === 'Scissors' && css`
+        border-color: #ecaa23;
+    ` }
+    ${({ value }) => value === 'Paper' && css`
+        border-color: #5574f7;
+    ` }
+    ${({ value }) => value === 'Rock' && css`
+        border-color: #dd405a;
+    ` }
+    ${({ value }) => value === 'Lizard' && css`
+        border-color: #8c5de5;
+    ` }
+    ${({ value }) => value === 'Spock' && css`
+        border-color: #58bbd2;
+    ` }
+    
 `
 
 
-export function Game({ img, value, setNewGame }) {
+export function Game({ img, value, setNewGame, visible }) {
     return (
-        <Content onClick={ () => setNewGame({img, value, isSelected: true}) }>
+        <Content onClick={() => setNewGame({ img, value, isSelected: true })} visible={visible} value={value}>
             <img src={img} title={value} />
         </Content>
     )
